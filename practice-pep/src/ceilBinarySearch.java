d91b3b1b2f3f3eab152754727906cdaf8e17c6b5
shivansu77 <shivansubisht77@gmail.com>
2026-01-23 22:28:49 +0530

    binary search in 2d array

--- practice-pep/src/ceilBinarySearch.java
+++ practice-pep/src/ceilBinarySearch.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class ceilBinarySearch {
+    public static void main(String[] args) {
+        int []arr={1, 2, 8, 10, 11, 12, 19};
+        int x=5;
+        System.out.println(findCeil(arr,x));
+    }
+    public static int findCeil(int[] arr, int x) {
+        // code here
+        int n=arr.length;
+        int s=0;
+        int e=n-1;
+        int min=Integer.MAX_VALUE;
+        while(s<=e){
+            int mid=(s)+(e-s)/2;
+            if(arr[mid]==x){
+                return mid;
+            }else if(arr[mid]<x){
+                s=mid+1;
+                min=Math.min(min,s);
+            }else{
+                e=mid;
+            }
+        }
+        return min;
+
+    }
 }
--- practice-pep/src/leetcode74.java
+++ practice-pep/src/leetcode74.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
-
 public class leetcode74 {
+    public static void main(String[] args) {
+        int [][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        int target=3;
+        System.out.println(searchMatrix(matrix,target));
+    }
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        //we will use binary search
+        int s=0;
+        int e=matrix.length-1;
+
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(matrix[m][0]>target){
+                e=m-1;
+            }else if(matrix[m][matrix[0].length-1]<target){
+                s=m+1;
+            }else{
+                int left=0,right=matrix[0].length-1;
+                while(left<=right){
+                    int mid2=(left)+(right-left)/2;
+                    if(matrix[m][mid2]<target){
+                        right=mid2+1;
+                    }else if(matrix[m][mid2]>target){
+                        left=mid2-1;
+                    }else{
+                        return true;
+                    }
+                }
+            }
+        }
+            return false;
+    }
 }
