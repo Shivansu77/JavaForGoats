3189f0d5af3daf9b74518964263ca10271563eb6
shivansu77 <shivansubisht77@gmail.com>
2026-01-25 21:48:10 +0530

    contest 2 q

--- practice-pep/src/cont1.java
+++ practice-pep/src/cont1.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class cont1 {
+    static void main(String[] args) {
+        int []nums={1,2,3,4};
+        System.out.println(minimumPrefixLength(nums));
+    }
+    public static int minimumPrefixLength(int[] nums) {
+        int n=nums.length;
+        int []arr = new int[n];
+        int i=n-2;
+        int index=0;
+        while(i>=0){
+            if(nums[i]<nums[i+1]){
+                i--;
+            }else{
+                index = i+1;
+                break;
+            }
+        }
+        return index;
+    }
 }
--- practice-pep/src/cont2.java
+++ practice-pep/src/cont2.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class cont2 {
+    public static void main(String[] args) {
+        int[] nums = {5, 4, -9, 6};
+        int k = 2;
+
+        int[] result = rotateElements(nums, k);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] rotateElements(int[] nums, int k) {
+        int n = nums.length;
+        ArrayList<Integer> positives = new ArrayList<>();
+
+        // collect positive elements
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                positives.add(nums[i]);
+            }
+        }
+
+        // rotate positives to the right by k
+        k = k % positives.size();
+        Collections.rotate(positives, k);
+
+        // put rotated positives back into nums
+        int posIndex = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                nums[i] = positives.get(posIndex++);
+            }
+        }
+
+        return nums;
+    }
 }
