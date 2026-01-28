33371540e531972b55589a991980d6fd39b6cab8
shivansu77 <shivansubisht77@gmail.com>
2026-01-28 18:07:12 +0530

    skip app but not apple

--- practice-pep/src/Stream.java
+++ practice-pep/src/Stream.java
@@ -1,6 +1,8 @@
 public class Stream {
     public static void main(String[] args) {
         skip("","baddaah");
+        skipApple("","mappleguysapp");
+
     }
     public static void skip(String p,String up){
         if(up.isEmpty()){
@@ -14,5 +16,33 @@ public class Stream {
             skip(p+ch,up.substring(1));
         }
     }
+
+    public static void skipApple(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return ;
+        }
+
+        if(up.startsWith("apple")){
+            skipApple(p,up.substring(5));
+        }else{
+            skipApple(p+up.charAt(0),up.substring(1));
+        }
+    }
+
+    public static void skipAppButNotAPPLE(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return ;
+        }
+
+        if(up.startsWith("app") && !up.startsWith("apple")){
+            skipAppButNotAPPLE(p,up.substring(3));
+        }else{
+            skipAppButNotAPPLE(p+up.charAt(0),up.substring(1));
+        }
+    }
+
+
 
 }
